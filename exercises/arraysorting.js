8950b6487433712631a39ce7ac480d005ab3019d
redwankibria <86778079+RedwanKibria@users.noreply.github.com>
2022-09-28 22:15:23 +0000

    solved the problem

--- exercises/arraysorting.js
+++ exercises/arraysorting.js
@@ -5,8 +5,22 @@
 // Write a function that takes in an array and sort the numbers inside from least to greatest
 
 function sortArray (array) {
-  
+  let x = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i; j < array.length; j++) {
+      if (array[j] < array[i]) {
+        x = array[j];
+        array[j] = array[i];
+        array[i] = x;
+      }
+    }
+  }
+  return array;
 }
+
+const array = [1, 6, 2, 4, 56, 67, 7, 5, 3, 7, 8, 43, 23, 4];
+console.log(sortArray(array));
 
 // BONUS sort the array without using .sort()
 
